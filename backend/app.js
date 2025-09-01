ebc73aefe79c0232e84b74bc38dbc8fa2fc088b5
Faraaz Ahmed <faraaz611@gmail.com>
2025-09-01 23:13:46 +0530

    fixed CORS issue

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,12 @@ const app = express();
 
 dotenv.config();
 
-// In Netlify Functions we are on the same origin; allow all origins to avoid blocking
-app.use(cors());
+app.use(cors({
+  origin: 'https://lavish-salon.vercel.app/',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+
+}));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 
